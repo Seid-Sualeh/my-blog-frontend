d40e401ee011dc10d7cd92907407f4e01c35a350
Abu-muaz <plshireseid@gmail.com>
2025-11-19 18:06:58 +0300

    add get all blogs

--- client/src/service/blog/get-all-blogs.js
+++ client/src/service/blog/get-all-blogs.js
@@ -1,1 +1,21 @@
 
+import axios from "axios";
+import { API_CONFIG } from "../../utils/configs/api-config";
+
+const getBlogs = async (params = {}) => {
+  try {
+    const response = await axios.get(
+      `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.BLOGS}`,
+      {
+        params,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Failed to fetch blogs");
+  }
+};
+export default getBlogs;
+
+
+
