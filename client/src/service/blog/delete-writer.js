3a489ba6e68c90eca460285b1c1432cf3449dbe9
Abu-muaz <plshireseid@gmail.com>
2025-11-19 18:05:48 +0300

    add delete writer

--- client/src/service/blog/delete-writer.js
+++ client/src/service/blog/delete-writer.js
@@ -1,1 +1,14 @@
 
+import axios from "axios";
+import { API_CONFIG } from "../../utils/configs/api-config";
+
+export const deleteWriter = async (id) => {
+  try {
+    const response = await axios.delete(
+      `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.WRITERS}/${id}`
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Failed to delete writer");
+  }
+};
