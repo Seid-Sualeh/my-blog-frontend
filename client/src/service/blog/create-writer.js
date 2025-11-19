3eb2209f3977f69cf96b4de8e7af63837c9f1840
Abu-muaz <plshireseid@gmail.com>
2025-11-19 18:03:50 +0300

    add create writer functionality

--- client/src/service/blog/create-writer.js
+++ client/src/service/blog/create-writer.js
@@ -1,1 +1,15 @@
 
+import axios from "axios";
+import { API_CONFIG } from "../../utils/configs/api-config";
+
+export const createWriter = async (writerData) => {
+  try {
+    const response = await axios.post(
+      `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.WRITERS}`,
+      writerData
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Failed to create writer");
+  }
+};
