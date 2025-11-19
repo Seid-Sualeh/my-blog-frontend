cbeff31d955dd44bca523cfc8afe4a6aab6a5de0
Abu-muaz <plshireseid@gmail.com>
2025-11-19 18:08:42 +0300

    add update writer

--- client/src/service/blog/update-writer.js
+++ client/src/service/blog/update-writer.js
@@ -1,1 +1,15 @@
 
+import axios from "axios";
+import { API_CONFIG } from "../../utils/configs/api-config";
+
+export const updateWriter = async (id, writerData) => {
+  try {
+    const response = await axios.put(
+      `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.WRITERS}/${id}`,
+      writerData
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Failed to update writer");
+  }
+};
