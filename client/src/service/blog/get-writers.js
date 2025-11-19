5f572e2b678f23061e82fd5d1ea926c0f2b0656b
Abu-muaz <plshireseid@gmail.com>
2025-11-19 18:07:34 +0300

    add get writer

--- client/src/service/blog/get-writers.js
+++ client/src/service/blog/get-writers.js
@@ -1,1 +1,17 @@
 
+import axios from "axios";
+import { API_CONFIG } from "../../utils/configs/api-config";
+
+export const getWriters = async (params = {}) => {
+  try {
+    const response = await axios.get(
+      `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.WRITERS}`,
+      {
+        params,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Failed to fetch writers");
+  }
+};
