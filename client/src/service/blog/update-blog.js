e1d04f98f43277563ebac416a959342fd3666abd
Abu-muaz <plshireseid@gmail.com>
2025-11-19 18:08:04 +0300

    add update bog

--- client/src/service/blog/update-blog.js
+++ client/src/service/blog/update-blog.js
@@ -1,1 +1,15 @@
 
+import axios from "axios";
+import { API_CONFIG } from "../../utils/configs/api-config";
+
+export const updateBlog = async (id, blogData) => {
+  try {
+    const response = await axios.put(
+      `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.BLOGS}/${id}`,
+      blogData
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Failed to update blog");
+  }
+};
